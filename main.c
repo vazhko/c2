498e824e6d07ac57fed460173b9b9155f79d71d2
Vladislav Vazhko <vazhkou@gmail.com>
2023-12-02 15:49:57 +0200

    доопрацювання

--- main.c
+++ main.c
@@ -6,14 +6,35 @@
 int main(int argc, char **argv){
     //LCD_1IN28_test();
 
-    //const char *path = "./LCD_1inch28_1.bmp";
+    char i;
+
+    const char *path1 = "./LCD_1inch28_1.bmp";
+    const char *path2 = "./LCD_1inch28_2.bmp";
+    const char *path3 = "./LCD_1inch28_3.bmp";
     if( argc == 2 ) {
         loadBmp(argv[1]);
     } else {         
         //loadBmp(path);
     }
     while (1){
-        getTouch();
+        if(getTouch()){
+            switch (i){
+                case 0:
+                    loadBmp(path1);
+                    break;
+                case 1:
+                    loadBmp(path2);
+                    break;
+                case 2:
+                    loadBmp(path3);
+                    break;   
+                case 3:
+                    clearLcd();
+                    break;                  
+            }
+            i++;
+            if(i > 3) i = 0;
+        }
     }
     return 0;
 }
--- test.h
+++ test.h
@@ -14,11 +14,12 @@
 #ifndef _TRST_H_
 #define _TRST_H_
 
-void LCD_1IN28_test(void);
-void Touch_HandWriting();
-void Touch_Gesture();
+//void LCD_1IN28_test(void);
+//void Touch_HandWriting();
+//void Touch_Gesture();
 void Touch_INT_callback(void);
 char loadBmp(const char *path);
 char getTouch();
+void clearLcd();
 
 #endif
